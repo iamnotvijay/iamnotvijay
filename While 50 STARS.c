3b6a8cc1c139ce3d45267ed1734592df8af7467f
iamnotvijay <95211610+iamnotvijay@users.noreply.github.com>
2021-12-20 14:40:24 +0530

    Add files via upload

---
+++ While 50 STARS.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+    while(i<=n)
+    {
+    	printf("*");
+    	i++;
+	}
+}
---
+++ While loop.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int i=1,n;
+	printf("Print all members from 1 to n");
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+}
